e53a4e1cefef0aff0e8579f4ed9172fb299992bf
ABHCH <chaturvedia435@gmail.com>
2023-03-12 11:33:22 +0530

    some changes

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 
 const Main = (props) => {
-    const copyText = () => {
-        let newTxt = document.getElementById('exampleFormControlTextarea1')
-        newTxt.select();
-        newTxt.setSelectRange(0,99999)
-        navigator.clipboard.writeText(newTxt.value);
-        alert("Copied to clipboard successfully")
-      };
+  const copyText = () => {
+    let newTxt = document.getElementById("exampleFormControlTextarea1");
+    newTxt.select();
+    newTxt.setSelectRange(0, 99999);
+    navigator.clipboard.writeText(newTxt.value);
+    alert("Copied to clipboard successfully");
+  };
   return (
     <>
-      <div className={`main-container my-4`}>
+      <div
+        style={{
+          backgroundColor: props.Mode === "dark" ? "black" : "white",
+          color: props.Mode === "dark" ? "white" : "black",
+          height: "100vh",
+          width: "100%",
+          padding: "1rem",
+        }}
+        className={`main-container my-4`}
+      >
         <div className="mb-3">
           <label
             htmlFor="exampleFormControlTextarea1"
@@ -26,19 +35,41 @@ const Main = (props) => {
             onChange={(event) => props.setText(event.target.value)}
           ></textarea>
         </div>
-        <button onClick={props.convertToUpperCase} className="btn btn-secondary">Convert to uppercase</button>
-        <button onClick={props.convertToLowerCase} className="btn btn-secondary mx-2">Covert to lowerCase</button>
-        <button onClick={props.removeSpace} className="btn btn-secondary mx-2">Remove Space</button>
-        <button onClick={copyText} className="btn btn-secondary mx-2">Copy Text</button>
-        <button onClick={props.clearText} className="btn btn-secondary mx-2">Clear Text</button>
+        <button
+          onClick={props.convertToUpperCase}
+          className="btn btn-secondary"
+        >
+          Convert to uppercase
+        </button>
+        <button
+          onClick={props.convertToLowerCase}
+          className="btn btn-secondary mx-2"
+        >
+          Covert to lowerCase
+        </button>
+        <button onClick={props.removeSpace} className="btn btn-secondary mx-2">
+          Remove Space
+        </button>
+        <button onClick={copyText} className="btn btn-secondary mx-2">
+          Copy Text
+        </button>
+        <button onClick={props.clearText} className="btn btn-secondary mx-2">
+          Clear Text
+        </button>
         <div className="word-counter-details my-3">
           <span className="total-words">Total Words : {props.cntWords}</span>
           <br />
-          <span className="total-character">Total Characters : {props.Text.length}</span>
+          <span className="total-character">
+            Total Characters : {props.Text.length}
+          </span>
           <br />
-          <span className="Reading-time">Reading Time : {props.ReadingTime}s</span>
+          <span className="Reading-time">
+            Reading Time : {props.ReadingTime}s
+          </span>
           <br />
-          <span className="speaking-time">Speaking Time : {props.speakTime}s</span>
+          <span className="speaking-time">
+            Speaking Time : {props.speakTime}s
+          </span>
         </div>
       </div>
     </>
